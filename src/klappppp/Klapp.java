f89d19a36e6b739b18099a336d39a004540c602b
remi128 <mremme@braintags.de>
2015-03-25 14:25:37 +0100

    message

---
+++ src/klappppp/Klapp.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package klappppp;
+
+/**
+ * @author mremme
+ *
+ */
+public class Klapp {
+
+	/**
+	 * 
+	 */
+	public Klapp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println( "chack" );
+
+	}
+
+}
